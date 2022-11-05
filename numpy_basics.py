cfa0d5a44c83807b3631efc6cdb9c29b2a714dd8
ajaypraj <34944605+ajaypraj@users.noreply.github.com>
2022-11-05 21:57:26 +0530

    Update numpy_basics.py

--- numpy_basics.py
+++ numpy_basics.py
@@ -2,7 +2,12 @@ import numpy as np
 """
 Numpy is python library for array handling.
 Rank is called as ==Dimension of an array
+Creating numpy array
+
+pip install numpy or 
+python -m pip install numpy
 """
+# 1. Creating array using list
 arr=np.array([1,2,3,4])
 print("Array with Rank 1",arr)
 
@@ -11,19 +16,21 @@ print("Array with Rank 1",arr)
 arr2=np.array([[1,2,3,4],[5,6,7,8]])
 print("Array with Rank 2", arr2)
 
-# Creating an array from numpy tuple
+# 2. Creating an array from numpy tuple
 
-arr3=np.array((1,2,3,))
+arr3=np.array((1,2,3))
 print("Arary created from tuple", arr3)
 
-# Slicing in Numpy array
-# Slicing in similar to slicing in Python
+
+
+# # Slicing in Numpy array
+# # Slicing in similar to slicing in Python list
 
 arr4=np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(arr4[:1,:1])  # will print first row and first columnn element so only 1 will be printed
+print(arr4[:2,:2])  # will print first row and first columnn element so only 1 will be printed
 
 
-# Printing the array Information 
+# # Printing the array Information 
 print("Type  of array",type(arr4))
 print("Size of array",str(arr4.size))
 print("Shape of the array", str(arr4.shape))
@@ -77,6 +84,64 @@ print(np.zeros((2,5)).reshape(5,2).flatten())
 a=np.array([[1,2,3],[4,5,6]], dtype='float')
 print("Adding to each elemnt of array",a+1)
 print("Subtracting 1 from each element", a-1)
+
+# Unary operation on array
+arr=np.array([[1,20,3],[4,3,4],[4,1,2]])
+print("Largest Element is ", arr.max())
+print("row wise maximum element is ", arr.max(axis=1))
+print("Coulmn wise maximum element is ", arr.max(axis=0))
+print("Sum of all element is ", arr.sum())
+print("Sum of element along row is ", arr.sum(axis=1))
+
+
+# How to perform math operation like addition, subtraction, multiplication in numpy
+
+# Define two numpy arrays
+arr1=np.array([[1,2,3],[4,5,6],[7,8,9]])
+arr2=np.array([[1,2,1],[2,1,3],[1,2,1]])
+
+# addition
+print("Addition",arr1 + arr2)
+
+# Subtraction
+print("Subtration",arr1 + arr2)
+
+# Multiplication
+print("Multiplication",arr1*arr2)
+
+# finding the sum of array element
+print("Sum",np.sum(arr1))
+
+# find squareroot of each element
+print("Sqrt",np.sqrt(arr1))
+
+# Transpose of an array 
+print("Transpose",arr1.T)
+
+
+# flatten method and ravel method
+# let take a matrix and flateen the matix 
+a=np.array([(1,2,3,4),(1,3,2,5)])
+x=a.flatten()
+print("Flattened Matrix",x)
+
+# lets change the element of flatten matrix and see the impact on original matrix
+x[2]=111
+print(x)
+print(a)
+# Flatten matrix's lelemnt will be chnages and original matrix wll not change . It shows that 
+# we have flatten matrix is deep copy of matrix a 
+
+# lets see the same example with ravel matrix
+# Here flatten matrix will be shallow copy of original matrix 
+
+y=a.ravel()
+print(y)
+y[1]=11
+print("original matrix a",a )
+print("Flatten matrix y",y)
+
+
 
 
 
